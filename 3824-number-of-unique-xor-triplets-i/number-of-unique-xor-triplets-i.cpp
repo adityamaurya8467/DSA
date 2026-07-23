7804d8795ca0bf6a38ed5ddfe306ca3ac2bb701a
adityamaurya8467 <adityamaurya8467@gmail.com>
2026-07-23 13:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 199 MB (9.38%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
-        if(n < 3)
-            return n;
-        int bits = 0;
-        int temp = n;
-        while(temp > 0) {
-            bits++;
-            temp /= 2;
-        }
-        return (1 << bits);
+        if(n <= 2) return n;
+        return 1 << bit_width(nums.size());
     }
 };
