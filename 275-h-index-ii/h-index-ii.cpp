9acdcbe0bc7508ce40c79fa0092e5a2e7ec9b74d
adityamaurya8467 <adityamaurya8467@gmail.com>
2026-07-23 13:39:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (97.84%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int low = 0, high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low);
+            int papers = n - mid;
+            if(citations[mid] == papers){
+                return papers;
+            }
+            else if(citations[mid] < papers){
+                low  = mid + 1;
+            }
+            else 
+            high = mid - 1;
+        }
+        return n -low;
+    }
+};
