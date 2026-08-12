fb53981d7d7fdccc8a0c8516eb236a635f81d369
adityamaurya8467 <adityamaurya8467@gmail.com>
2026-08-12 21:37:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (12.77%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        // for set the first colms are 1's
+        for(int i = 0; i < rows; i++){
+            if(grid[i][0] == 0){ //flip karenge
+                for(int j = 0; j < cols; j++){
+                    if(grid[i][j]==0) grid[i][j] = 1;
+                    else grid[i][j] = 0;
+                }
+            }
+        }
+        //find the cols where noz>noo
+        for(int j = 0; j < cols; j++){
+            int noz = 0;
+            int noo = 0;
+            for(int i = 0; i < rows; i++){
+                if(grid[i][j] == 0) noz++;
+                else noo++;
+            }
+            if(noz > noo){
+                for(int i = 0; i < rows; i++){
+                    if(grid[i][j] == 0) grid[i][j] = 1;
+                    else grid[i][j] = 0;
+                }
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < rows; i++){
+            int x = 1;
+            for(int j = cols - 1; j >= 0; j--){
+                sum += grid[i][j] * x;
+                x *= 2;
+            }
+        }
+        return sum;
+    }
+};
