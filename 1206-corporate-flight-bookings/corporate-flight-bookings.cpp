f5a8e9f8ea20bec29b58744f694f57ce4d2bfec0
adityamaurya8467 <adityamaurya8467@gmail.com>
2026-08-26 20:35:46 +0530

    Time: 7 ms (46.06%) | Memory: 72.9 MB (43.72%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        int first = 0;
+        int last = n-1;
+        int seat = 0;
+        vector<int> diff(n + 1, 0);
+        vector<int> answer(n);
+        for(auto &booking : bookings) {
+            int first = booking[0] - 1;
+            int last = booking[1] - 1;
+            int seats = booking[2];
+            diff[first] += seats;
+            diff[last+1] -= seats;
+        }
+        int current = 0;
+        for(int i = 0; i < n; i++){
+            current += diff[i];
+            answer[i] = current;
+        }
+        return answer;
+
+    }
+};
