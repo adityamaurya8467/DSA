7622a20d26a425db79a008b89fc0ef45023cbd0a
adityamaurya8467 <adityamaurya8467@gmail.com>
2026-09-03 22:20:11 +0530

    Time: 202 ms (48.22%) | Memory: 99.5 MB (24.24%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* curr1 = list1;
+        a--;
+        while(a--){
+            curr1 = curr1->next;
+        }
+        b++;
+        ListNode* curr2 = list1;
+        while(b--){
+            curr2 = curr2->next;
+        }
+        ListNode* temp = list2;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = curr2;
+        curr1->next = list2;
+        return list1;
+    }
+};
