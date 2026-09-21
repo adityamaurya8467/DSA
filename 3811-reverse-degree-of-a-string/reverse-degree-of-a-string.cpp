ceaaf1a912910ad5e4cbbc804884e2473d6dd7e1
adityamaurya8467 <adityamaurya8467@gmail.com>
2026-09-22 00:12:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.66%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++){
+            int ascii = s[i];
+            int reverseValue = 123 - ascii;
+            ans += reverseValue * (i + 1);
+        }
+        return ans;
+    }
+};
